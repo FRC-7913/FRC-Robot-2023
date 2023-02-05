927f9ab76de101c999ca32b6540418de2f2b00fc
Evan <53529192+BruceMcRooster@users.noreply.github.com>
2023-02-05 18:15:52 -0500

    Added encoders, gyro, and odometry to DriveSubsystem

--- src/main/java/frc7913/robot/subsystems/DriveSubsystem.kt
+++ src/main/java/frc7913/robot/subsystems/DriveSubsystem.kt
@@ -1,6 +1,10 @@
 package frc7913.robot.subsystems
 
+import edu.wpi.first.math.kinematics.DifferentialDriveOdometry
+import edu.wpi.first.wpilibj.ADXRS450_Gyro
+import edu.wpi.first.wpilibj.Encoder
 import edu.wpi.first.wpilibj.drive.DifferentialDrive
+import edu.wpi.first.wpilibj.interfaces.Gyro
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup
 import edu.wpi.first.wpilibj.motorcontrol.Spark
 import edu.wpi.first.wpilibj2.command.SubsystemBase
@@ -29,5 +33,33 @@ object DriveSubsystem : SubsystemBase() {
     init {
         driveTrain.isSafetyEnabled = true
         driveTrain.expiration = 0.1
+    }
+
+    private val leftEncoder = Encoder(
+        PortConstants.leftEncoder.first,
+        PortConstants.leftEncoder.second,
+        DriveConstants.leftEncoderInverted,
+    )
+    private val rightEncoder = Encoder(
+        PortConstants.rightEncoder.first,
+        PortConstants.rightEncoder.second,
+        DriveConstants.rightEncoderInverted,
+    )
+
+    private val gyro: Gyro = ADXRS450_Gyro()
+
+    init {
+        leftEncoder.distancePerPulse = DriveConstants.encoderDistancePerPulse
+        rightEncoder.distancePerPulse = DriveConstants.encoderDistancePerPulse
+
+        resetEncoders()
+    }
+
+    private val odometry = DifferentialDriveOdometry(gyro.rotation2d, leftEncoder.distance, rightEncoder.distance)
+
+    override fun periodic() {
+        odometry.update(
+            gyro.rotation2d, leftEncoder.distance, rightEncoder.distance
+        )
     }
 }
